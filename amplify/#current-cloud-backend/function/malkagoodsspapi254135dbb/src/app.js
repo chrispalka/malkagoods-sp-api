0959632154d77fb97200e8ae11244223a6b3d378
Chris Palka <cpalka87@gmail.com>
2025-09-12 15:15:19 -0600

    update

--- amplify/#current-cloud-backend/function/malkagoodsspapi254135dbb/src/app.js
+++ amplify/#current-cloud-backend/function/malkagoodsspapi254135dbb/src/app.js
@@ -65,7 +65,6 @@ app.use(function (req, res, next) {
 
 app.get('/getInventory', async function (req, res) {
   const secretValue = await getSecret();
-  console.log('****', secretValue);
   const spClientSecret = secretValue.SecretString
     ? JSON.parse(secretValue.SecretString)['SP_CLIENT_SECRET']
     : null;
